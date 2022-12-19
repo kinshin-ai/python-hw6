9e8f450834ed8f2c490d1eb318e2d99a2809f9d7
kinshin-ai <108364024+kinshin-ai@users.noreply.github.com>
2022-12-19 11:24:58 +0000

    переместил функцию вверх

--- task1.py
+++ task1.py
@@ -1,17 +1,16 @@
 #Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
-list_size = int(input('введите размер списка: '))
-list = []
-for i in range(list_size):
-  list.append(int(input('введите число: ')))
-print (list)
-
-
 def summarize (lst):
-  total = 0
+  total=0
   for x in range (len(lst)):
     if x%2 != 0:
       total =  total + lst[x]
   return total
+
+list_size = int(input('введите размер списка: '))
+list=[]
+for i in range(list_size):
+  list.append(int(input('введите число: ')))
+print (list)
 
 print(summarize(list))
